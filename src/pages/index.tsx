705bd7bab9601d38adf2a0b7fac9bb9889413a21
Bryan Personal <bryanlohjw97@gmail.com>
2023-03-27 14:35:55 +0800

    feat: post and profile page routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-
+import { useState } from "react";
 import { api, type RouterOutputs } from "~/utils/api";
-
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage, LoadingSpinner } from "~/components/Loading";
-import { useState } from "react";
 import { toast } from "react-hot-toast";
+
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -93,10 +93,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col ">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">{` · ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` · ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
